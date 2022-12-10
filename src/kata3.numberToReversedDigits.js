aed7b40e24243b0094c11cd5afa4a7de1753c2a6
Abi Turner <turner.abi@gmail.com>
2022-12-10 14:22:02 +0000

    completed numbersToReversedDigits test

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,20 +1,5 @@
-//const numberToReversedDigits = (number) => {
-//    const numberToReverse = number;
-//    const arrayValues = Array.from(numberToReverse);
-//   return arrayValues.reverse();
-
-  // code to put the number passed into an array of digits and reverse them//
-// };
-
-//I need to make make the number into an array.
-// I then need to reverse the array. 
-
-const numberToReversedDigits = (numbers) => {
-  function numbersToString(numbers) {
-        return numbers.toString().split(",");
-    };
-  const reverseString = (numbersToString) =>
-    numbersToString.map(word.split("").reverse().join(""));
+const numberToReversedDigits = (number) => {
+ const numberToString = String(number).split("").reverse().map(Number);
+ return numberToString;
 };
-
 module.exports = numberToReversedDigits;
